a09254b3af890c02359df2c593a526300921e034
edus44 <eduhhg@gmail.com>
2017-06-21 12:57:14 +0200

    clean and doc

--- index.js
+++ index.js
@@ -2,30 +2,62 @@
 import keycode from 'keycode'
 import EventEmitter from 'eventemitter3'
 
-function WasdMixinFactory(bus){
-    return {
-        created:bindHook(bus,'on'),
-        beforeDestroy:bindHook(bus,'removeListener')
-    }
-}
+/**
+ * Wasd Mixin to listen to lifecycle hooks
+ * @type {Object}
+ */
+const wasdMixin = {
 
-function bindHook(bus,fn){
-    return function(){
+    /**
+     * Initialize events preset at $options.wasd
+     * @return {void}
+     */
+    created(){
         if (typeof this.$options.wasd == 'object'){
             for(let evName in this.$options.wasd){
-                bus[fn](evName,this.$options.wasd[evName].bind(this))
+                this.$wasd(evName,this.$options.wasd[evName])
+            }
+        }
+    },
+
+    /**
+     * Remove listeners saved in _wasdListeners
+     * @return {void}
+     */
+    beforeDestroy(){
+        if (this._wasdListeners && this._wasdListeners.length){
+            for(let i in this._wasdListeners){
+                bus.removeListener(this._wasdListeners[i][0],this._wasdListeners[i][1])
             }
         }
     }
 }
 
+/**
+ * Listen to specific key event and
+ * save the listener for latter removing
+ * 
+ * @param  {String}   evName Key event name with optional suffix (~Press or ~Release)
+ * @param  {Function} fn     Bound function, context is forced to vm
+ * @return {void}
+ */
+function wasdBind(evName,fn){
+    fn = fn.bind(this)
+    this._wasdBus.on(evName,fn)
 
-export default Vue => {
+    if (!this._wasdListeners) this._wasdListeners = []
 
-    let bus = new EventEmitter()
+    this._wasdListeners.push([evName,fn])
+}
+
+
+/**
+ * Start listening to window key events
+ * @param  {EventEmitter} bus
+ * @return {void}
+ */
+function attachGlobalListener(bus){
     let pressed = {}
-
-    Vue.mixin(WasdMixinFactory(bus))
 
     window.addEventListener('keydown',(e)=>{
         let k = keycode(e.keyCode)
@@ -41,4 +73,21 @@ export default Vue => {
         bus.emit(k+'Release',Date.now()-pressed[k])
         delete pressed[k]
     })
+}
+
+/**
+ * Plugin install
+ * @param  {Vue} Vue 
+ * @return {void}
+ */
+export default Vue => {
+
+    // Avoid multiple installs
+    if (Vue.prototype._wasdBus) return
+
+    Vue.prototype._wasdBus = new EventEmitter() // Internal event bus
+    Vue.prototype.$wasd = wasdBind
+    Vue.mixin(wasdMixin)
+
+    attachGlobalListener(Vue.prototype._wasdBus)
 }
